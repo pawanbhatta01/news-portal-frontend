cb1d652030cc22b3f963282fd35063f428a575d6
pawanbhatta01 <pawanrajbhatta.xdezo@outlook.com>
2023-05-18 13:45:42 +0545

    Created news,contact page, dynamic active navitem.

--- assets/js/min.js
+++ assets/js/min.js
@@ -6,3 +6,18 @@ function myFunction() {
         x.className = "topnav";
     }
 }
+
+
+var uri = window.location.pathname;
+var url = window.location.origin;
+var elements = document.querySelectorAll('.nav-links');
+
+for (const element of elements) {
+    var link = element.href.replace(`${url}`, '');
+    if (uri == link) {
+        document.querySelector(`[href="${link}"]`).className += " active";
+    }
+    if (uri == "/") {
+        document.querySelector(`[href="/index.html"]`).className += " active";
+    }
+} 
